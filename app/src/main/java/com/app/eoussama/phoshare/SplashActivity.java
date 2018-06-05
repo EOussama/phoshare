31517301c7112d4c95253a6f16c4075c7100c07d
Eoussama <oussama_gtacj@live.fr>
2018-06-05 14:30:33 +0000

    Splash screen timeout added

--- app/src/main/java/com/app/eoussama/phoshare/SplashActivity.java
+++ app/src/main/java/com/app/eoussama/phoshare/SplashActivity.java
@@ -1,13 +1,25 @@
 package com.app.eoussama.phoshare;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int SPLASH_SCREEN_TIMEOUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(loginIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
     }
 }
