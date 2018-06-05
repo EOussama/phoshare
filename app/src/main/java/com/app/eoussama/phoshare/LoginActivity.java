fd6c539a6a637c704a264447acb2cf56bd5966f3
Eoussama <oussama_gtacj@live.fr>
2018-06-05 15:38:28 +0000

    Some java code added

--- app/src/main/java/com/app/eoussama/phoshare/LoginActivity.java
+++ app/src/main/java/com/app/eoussama/phoshare/LoginActivity.java
@@ -2,12 +2,26 @@ package com.app.eoussama.phoshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignup = (Button) this.findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                LoginActivity.this.startActivity(signupIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/app/eoussama/phoshare/SignupActivity.java
+++ app/src/main/java/com/app/eoussama/phoshare/SignupActivity.java
@@ -2,12 +2,47 @@ package com.app.eoussama.phoshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.CheckBox;
 
 public class SignupActivity extends AppCompatActivity {
+
+    Button btnSignup, btnClear;
+    EditText etUsername, etPassword, etPasswordConfirmation, etSecurityAnswer;
+    Spinner sSecurityQuestions;
+    CheckBox cbTerms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btnSignup = (Button) this.findViewById(R.id.btnSignup);
+        btnClear = (Button) this.findViewById(R.id.btnClear);
+
+        etUsername = (EditText) this.findViewById(R.id.etUsername);
+        etPassword  = (EditText) this.findViewById(R.id.etPassword);
+        etPasswordConfirmation  = (EditText) this.findViewById(R.id.etPasswordConfirmation);
+        etSecurityAnswer = (EditText) this.findViewById(R.id.etSecurityAnswer);
+
+        cbTerms = (CheckBox) this.findViewById(R.id.cbTerms);
+        sSecurityQuestions = (Spinner) this.findViewById(R.id.etPasswordConfirmation);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
